995ed6387da179ef73a9b0e11d2120ba43eddd01
FizzyApple12 <davideroeder@gmail.com>
2020-04-05 03:01:49 -0400

    update hamburger to use linux file paths

--- commands/general/_hamburger.js
+++ commands/general/_hamburger.js
@@ -54,26 +54,26 @@ exports.run = function (bot, msg, args) {
         var validOrder = true;
 
         var newDir = "";
-        var newDirA = __dirname.split('\\');
+        var newDirA = __dirname.split('/');
         newDirA.pop();
         newDirA.pop();
         newDirA.forEach(dir => {
-            newDir += dir + "\\"
+            newDir += dir + "/"
         });
 
         for (var i = 0; i < input.length; i++) {
             if (input[i] == "h" && input[i+1] == "a" && input[i+2] == "m") {
-                order.push(newDir+"hamburger\\ham.wav");
+                order.push(newDir+"hamburger/ham.wav");
                 i += 2;
             } else if (input[i] == "g" && input[i+1] == "e" && input[i+2] == "r") {
-                order.push(newDir+"hamburger\\ger.wav");
+                order.push(newDir+"hamburger/ger.wav");
                 i += 2;
             } else if (input[i] == "b") {
-                order.push(newDir+"hamburger\\b.wav");
+                order.push(newDir+"hamburger/b.wav");
             } else if (input[i] == "u") {
-                order.push(newDir+"hamburger\\u.wav");
+                order.push(newDir+"hamburger/u.wav");
             } else if (input[i] == "r") {
-                order.push(newDir+"hamburger\\r.wav");
+                order.push(newDir+"hamburger/r.wav");
             } else {
                 msg.channel.send(`\`\`\`\n${input}\r${" ".repeat(i)}^ Cannot find ingredient "${input[i]}"\n\`\`\``);
                 validOrder = false;
